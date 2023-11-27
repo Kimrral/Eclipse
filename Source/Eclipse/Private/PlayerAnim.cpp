da566debecf1f2ac177df4201d66f3d666d9b3a5
Kimrral <kimhhyunjjun@gmail.com>
2023-11-27 19:55:14 +0900

    Feat : Fire Function2

--- Source/Eclipse/Private/PlayerAnim.cpp
+++ Source/Eclipse/Private/PlayerAnim.cpp
@@ -3,3 +3,25 @@
 
 #include "PlayerAnim.h"
 
+#include "PlayerCharacter.h"
+
+void UPlayerAnim::NativeBeginPlay()
+{
+	Super::NativeBeginPlay();
+
+	me =  Cast<APlayerCharacter>(TryGetPawnOwner());
+
+}
+
+void UPlayerAnim::AnimNotify_ReloadStart()
+{
+	GetWorld()->GetTimerManager().ClearTimer(me->shootEnableHandle);
+	me->CanShoot=false;
+}
+
+void UPlayerAnim::AnimNotify_ReloadEnd()
+{
+	me->curRifleAmmo=30;
+	me->CanShoot=true;
+
+}

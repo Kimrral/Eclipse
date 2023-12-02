8b7a2c028df02e1ce9b2ed1421bc8fbe3c01f25e
Kimrral <kimhhyunjjun@gmail.com>
2023-12-02 18:51:19 +0900

    Feat : Hit Aim Anim

---
+++ Source/Eclipse/Public/CrosshairWidget.h
@@ -0,0 +1,33 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Blueprint/UserWidget.h"
+#include "CrosshairWidget.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class ECLIPSE_API UCrosshairWidget : public UUserWidget
+{
+	GENERATED_BODY()
+
+public:
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, meta=(BindWidget))
+	class UImage* CrosshairImage;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, meta=(BindWidget))
+	class UImage* HitCrosshair;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, meta=(BindWidget))
+	class UImage* HeadHitCrosshair;
+	
+	UPROPERTY(BlueprintReadWrite, Transient, meta = (BindWidgetAnim))
+	class UWidgetAnimation* HitAppearAnimation;
+
+	UPROPERTY(BlueprintReadWrite, Transient, meta = (BindWidgetAnim))
+	class UWidgetAnimation* HeadHitAppearAnimation;
+	
+};

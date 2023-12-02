9a99117245de20bbca16b69bb39eb96f2d00795d
Kimrral <kimhhyunjjun@gmail.com>
2023-12-02 11:23:50 +0900

    Feat : Ammo Actors

---
+++ Source/Eclipse/Private/M249AmmoActor.cpp
@@ -0,0 +1,60 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "M249AmmoActor.h"
+
+#include "PlayerCharacter.h"
+#include "Components/SphereComponent.h"
+#include "Kismet/GameplayStatics.h"
+
+void AM249AmmoActor::BeginPlay()
+{
+	Super::BeginPlay();
+
+	ammoMesh->OnComponentBeginOverlap.AddDynamic(this, &AM249AmmoActor::OnOverlap);
+	playerTraceSphereCollision->OnComponentBeginOverlap.AddDynamic(this, &AM249AmmoActor::SphereOnOverlap);
+}
+
+void AM249AmmoActor::Tick(float DeltaSeconds)
+{
+	Super::Tick(DeltaSeconds);
+
+	if(bTracePlayer)
+	{
+		auto target = UGameplayStatics::GetActorOfClass(GetWorld(), APlayerCharacter::StaticClass());
+		if (target != nullptr)
+		{
+			// 플레이어의 위치 - 나의 위치 = 갈 방향을 설정한다.
+			FVector targetDir = target->GetActorLocation() - GetActorLocation();
+			targetDir.Normalize();
+			direction = targetDir;
+		}
+		SetActorLocation(GetActorLocation() + direction * moveSpeed * DeltaSeconds);
+	}
+}
+
+void AM249AmmoActor::OnOverlap(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult)
+{
+	if(OtherActor)
+	{
+		auto player=Cast<APlayerCharacter>(OtherActor);
+		if(player)
+		{
+			player->maxM249Ammo+=100;
+			this->Destroy();
+		}
+	}
+}
+
+void AM249AmmoActor::SphereOnOverlap(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult)
+{
+	if(OtherActor)
+	{
+		auto player=Cast<APlayerCharacter>(OtherActor);
+		if(player)
+		{
+			bTracePlayer=true;
+		}
+	}
+}
+
---
+++ Source/Eclipse/Private/RifleAmmoActor.cpp
@@ -0,0 +1,59 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "RifleAmmoActor.h"
+
+#include "PlayerCharacter.h"
+#include "Components/SphereComponent.h"
+#include "Kismet/GameplayStatics.h"
+
+void ARifleAmmoActor::BeginPlay()
+{
+	Super::BeginPlay();
+
+	ammoMesh->OnComponentBeginOverlap.AddDynamic(this, &ARifleAmmoActor::OnOverlap);
+	playerTraceSphereCollision->OnComponentBeginOverlap.AddDynamic(this, &ARifleAmmoActor::SphereOnOverlap);
+}
+
+void ARifleAmmoActor::Tick(float DeltaSeconds)
+{
+	Super::Tick(DeltaSeconds);
+
+	if(bTracePlayer)
+	{
+		auto target = UGameplayStatics::GetActorOfClass(GetWorld(), APlayerCharacter::StaticClass());
+		if (target != nullptr)
+		{
+			// 플레이어의 위치 - 나의 위치 = 갈 방향을 설정한다.
+			FVector targetDir = target->GetActorLocation() - GetActorLocation();
+			targetDir.Normalize();
+			direction = targetDir;
+		}
+		SetActorLocation(GetActorLocation() + direction * moveSpeed * DeltaSeconds);
+	}
+}
+
+void ARifleAmmoActor::OnOverlap(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult)
+{
+	if(OtherActor)
+	{
+		auto player=Cast<APlayerCharacter>(OtherActor);
+		if(player)
+		{
+			player->maxRifleAmmo+=40;
+			this->Destroy();
+		}
+	}
+}
+
+void ARifleAmmoActor::SphereOnOverlap(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult)
+{
+	if(OtherActor)
+	{
+		auto player=Cast<APlayerCharacter>(OtherActor);
+		if(player)
+		{
+			bTracePlayer=true;
+		}
+	}
+}

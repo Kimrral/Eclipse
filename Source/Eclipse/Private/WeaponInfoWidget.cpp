d7c13e9db6ac6d61b9ace029a522d0c6440323e4
Kimrral <kimhhyunjjun@gmail.com>
2023-12-04 20:23:24 +0900

    Feat : Revert1

--- Source/Eclipse/Private/WeaponInfoWidget.cpp
+++ Source/Eclipse/Private/WeaponInfoWidget.cpp
@@ -3,3 +3,15 @@
 
 #include "WeaponInfoWidget.h"
 
+void UWeaponInfoWidget::NativeTick(const FGeometry& MyGeometry, float InDeltaTime)
+{
+	Super::NativeTick(MyGeometry, InDeltaTime);
+
+	weaponHoldPercent=FMath::Clamp(weaponHoldPercent-0.003, 0, 1);
+	
+	progressSlider->SetValue(weaponHoldPercent);
+	progressSlider_1->SetValue(weaponHoldPercent);
+	progressSlider_2->SetValue(weaponHoldPercent);
+	progressSlider_3->SetValue(weaponHoldPercent);
+
+}

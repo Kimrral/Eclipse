6d9b883994e347f3a2636ef297e91f16572aac8e
Kimrral <kimhhyunjjun@gmail.com>
2023-12-06 21:05:45 +0900

    Feat : Damage Floating Widget

--- Source/Eclipse/Private/AmmoActor.cpp
+++ Source/Eclipse/Private/AmmoActor.cpp
@@ -10,7 +10,7 @@
 AAmmoActor::AAmmoActor()
 {
  	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
-	PrimaryActorTick.bCanEverTick = true;
+	PrimaryActorTick.bCanEverTick = false;
 
 	ammoMesh=CreateDefaultSubobject<UStaticMeshComponent>(TEXT("weaponMesh"));
 	SetRootComponent(ammoMesh);
---
+++ Source/Eclipse/Private/DamageWidgetActor.cpp
@@ -0,0 +1,33 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "DamageWidgetActor.h"
+
+#include "Components/WidgetComponent.h"
+
+// Sets default values
+ADamageWidgetActor::ADamageWidgetActor()
+{
+ 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = false;
+
+	DamageWidgetComponent=CreateDefaultSubobject<UWidgetComponent>(TEXT("DamageWidgetComponent"));
+	SetRootComponent(DamageWidgetComponent);
+
+}
+
+// Called when the game starts or when spawned
+void ADamageWidgetActor::BeginPlay()
+{
+	Super::BeginPlay();
+
+	SetLifeSpan(1.5f);	
+}
+
+// Called every frame
+void ADamageWidgetActor::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+

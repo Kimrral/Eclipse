7abe7487eecef8653470e39e42bfd91e3658b779
Kimrral <kimhhyunjjun@gmail.com>
2023-12-31 09:43:53 +0900

    Fix : Respawn montage

--- Source/Eclipse/Private/PlayerCharacter.cpp
+++ Source/Eclipse/Private/PlayerCharacter.cpp
@@ -218,7 +218,7 @@ void APlayerCharacter::BeginPlay()
 	{
 		PlayerController->EnableInput(PlayerController);		
 	}
-	PlayAnimMontage(zoomingMontage, 1, FName("Respawn"));
+	//PlayAnimMontage(zoomingMontage, 1, FName("Respawn"));
 
 	auto playerCam = UGameplayStatics::GetPlayerCameraManager(GetWorld(), 0);
 	playerCam->StopCameraFade();

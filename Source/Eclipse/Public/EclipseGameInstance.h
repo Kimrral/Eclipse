ed15161d2c15dc6643071169446435f147276688
Kimrral <kimhhyunjjun@gmail.com>
2024-01-03 14:38:43 +0900

    Feat : Before GameInstance Test

---
+++ Source/Eclipse/Public/EclipseGameInstance.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Engine/GameInstance.h"
+#include "EclipseGameInstance.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class ECLIPSE_API UEclipseGameInstance : public UGameInstance
+{
+	GENERATED_BODY()
+	
+};

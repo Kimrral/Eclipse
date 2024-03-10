b2726e172f9b2e684ac716f313c5a0a65a9c2e51
Kimrral <kimhhyunjjun@gmail.com>
2024-03-10 14:28:55 +0900

    Feat : Level Selection Widget Class

--- Source/Eclipse/Private/QuitWidget.cpp
+++ Source/Eclipse/Private/QuitWidget.cpp
@@ -4,24 +4,19 @@
 #include "QuitWidget.h"
 
 #include "EclipsePlayerController.h"
-#include "PlayerCharacter.h"
 #include "Blueprint/WidgetBlueprintLibrary.h"
 #include "Components/Button.h"
-#include "Kismet/GameplayStatics.h"
 #include "Kismet/KismetSystemLibrary.h"
 
 void UQuitWidget::NativeConstruct()
 {
 	Super::NativeConstruct();
 
-	player = Cast<APlayerCharacter>(UGameplayStatics::GetPlayerCharacter(GetWorld(), 0));
 	pc=Cast<AEclipsePlayerController>(GetWorld()->GetFirstPlayerController());
 
 	QuitYes->OnClicked.AddDynamic(this, &UQuitWidget::QuitY);
 	QuitNo->OnClicked.AddDynamic(this, &UQuitWidget::QuitN);
 
-	LevelYes->OnClicked.AddDynamic(this, &UQuitWidget::LevelY);
-	LevelNo->OnClicked.AddDynamic(this, &UQuitWidget::LevelN);
 
 	quitBool=false;
 }
@@ -39,21 +34,3 @@ void UQuitWidget::QuitN()
 	this->RemoveFromParent();
 }
 
-void UQuitWidget::LevelY()
-{
-	if(quitBool==false)
-	{
-		quitBool=true;
-		UWidgetBlueprintLibrary::SetInputMode_GameOnly(pc);
-		pc->SetShowMouseCursor(false);
-		this->RemoveFromParent();
-		player->MoveToIsolatedShip();
-	}	
-}
-
-void UQuitWidget::LevelN()
-{
-	UWidgetBlueprintLibrary::SetInputMode_GameOnly(pc);
-	pc->SetShowMouseCursor(false);
-	this->RemoveFromParent();
-}

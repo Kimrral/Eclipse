f32904eea5b0501fb05f634465130e184340f8d0
Kimrral <kimhhyunjjun@gmail.com>
2024-04-05 16:22:27 +0900

    Feat : Enemy Class, Animation <Spider>

---
+++ Source/Eclipse/Animation/SpiderAnim.h
@@ -0,0 +1,21 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "EnemyAnim.h"
+#include "SpiderAnim.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class ECLIPSE_API USpiderAnim : public UEnemyAnim
+{
+	GENERATED_BODY()
+
+public:
+	UFUNCTION()
+	void AnimNotify_HitPoint();
+	
+};
---
+++ Source/Eclipse/Enemy/Spider.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Enemy.h"
+#include "Spider.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class ECLIPSE_API ASpider : public AEnemy
+{
+	GENERATED_BODY()
+	
+};

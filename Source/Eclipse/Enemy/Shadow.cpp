d7c7c30ec6de1a388d5d4972ec99056c6f66782e
Kimrral <kimhhyunjjun@gmail.com>
2024-07-23 18:02:29 +0900

    Feat : Source Code Description Script <Reward Manager Class>

--- Source/Eclipse/Enemy/Shadow.cpp
+++ Source/Eclipse/Enemy/Shadow.cpp
@@ -8,20 +8,25 @@
 
 AShadow::AShadow()
 {
-	// Enemy FSM
+	// 적 FSM 초기화
 	EnemyFSM = CreateDefaultSubobject<UEnemyFSM>(TEXT("EnemyFSM"));
 	
+	// 무기 컴포넌트 초기화 및 메쉬에 부착
 	WeaponComp = CreateDefaultSubobject<USkeletalMeshComponent>(TEXT("WeaponComp"));
-	WeaponComp->SetupAttachment(GetMesh(), FName("hand_r"));
+	WeaponComp->SetupAttachment(GetMesh(), FName("hand_r")); // 'hand_r' 소켓에 부착
 }
 
 void AShadow::BeginPlay()
 {
 	Super::BeginPlay();
 
+	// 무기 컴포넌트의 첫 번째 머티리얼을 가져옴
 	const auto Material = WeaponComp->GetMaterial(0);
+	
+	// 동적 머티리얼 인스턴스 생성
 	if(UMaterialInstanceDynamic* DynamicMaterial = UMaterialInstanceDynamic::Create(Material, this))
 	{
+		// 무기 컴포넌트에 동적 머티리얼 설정
 		WeaponComp->SetMaterial(0, DynamicMaterial);
 	}
 }
@@ -29,7 +34,13 @@ void AShadow::BeginPlay()
 void AShadow::SetDissolveValue(const float Value)
 {
 	Super::SetDissolveValue(Value);
+	
+	// 0에서 1 사이의 값을 보간하여 설정
 	const double Lerp = UKismetMathLibrary::Lerp(0, 1, Value);
+	
+	// 무기 컴포넌트의 첫 번째 머티리얼을 동적 머티리얼로 캐스팅
 	UMaterialInstanceDynamic* DynamicMaterial = Cast<UMaterialInstanceDynamic>(WeaponComp->GetMaterial(0));
+	
+	// 동적 머티리얼의 DissolveParams 파라미터 값을 설정
 	DynamicMaterial->SetScalarParameterValue("DissolveParams", Lerp);
 }
--- Source/Eclipse/Prop/RewardContainer.cpp
+++ Source/Eclipse/Prop/RewardContainer.cpp
@@ -9,50 +9,64 @@
 #include "Kismet/GameplayStatics.h"
 #include "Net/UnrealNetwork.h"
 
-// Sets default values
+// 기본 값을 설정합니다
 ARewardContainer::ARewardContainer()
 {
-	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	// 이 액터가 매 프레임마다 Tick()을 호출하도록 설정합니다. 필요하지 않은 경우 성능 향상을 위해 끌 수 있습니다.
 	PrimaryActorTick.bCanEverTick = true;
 	PrimaryActorTick.bStartWithTickEnabled = false;
 
-	// Root Mesh
+	// 루트 메쉬 컴포넌트를 생성하고 루트 컴포넌트로 설정합니다.
 	ContainerMesh = CreateDefaultSubobject<UGeometryCollectionComponent>(TEXT("containerMesh"));
 	SetRootComponent(ContainerMesh);
-	// Reward Manager
+	
+	// 보상 매니저 컴포넌트를 생성합니다.
 	RewardManager = CreateDefaultSubobject<URewardManagerComponent>(TEXT("RewardManagerComponent"));
 
+	// 현재 박스 HP를 최대 HP로 설정합니다.
 	CurBoxHP = MaxBoxHP;
+	
+	// 이 액터가 네트워크 상에서 복제되도록 설정합니다.
 	bReplicates = true;
 }
 
+// 이 액터의 복제된 속성을 설정합니다.
 void ARewardContainer::GetLifetimeReplicatedProps(TArray<FLifetimeProperty>& OutLifetimeProps) const
 {
 	Super::GetLifetimeReplicatedProps(OutLifetimeProps);
 
+	// IsBoxDestroyed 변수를 네트워크 상에서 복제되도록 설정합니다.
 	DOREPLIFETIME(ARewardContainer, IsBoxDestroyed);
 }
 
-// Called when the game starts or when spawned
+// 게임이 시작되거나 액터가 스폰될 때 호출됩니다
 void ARewardContainer::BeginPlay()
 {
 	Super::BeginPlay();
 
+	// 컨테이너 메쉬에 커스텀 뎁스 렌더링을 설정합니다.
 	ContainerMesh->SetRenderCustomDepth(true);
+	// 컨테이너 메쉬가 물리를 시뮬레이션하도록 설정합니다.
 	ContainerMesh->SetSimulatePhysics(true);
 }
 
+// 박스가 파괴되었을 때 호출됩니다
 void ARewardContainer::BoxDestroyed()
 {
+	// 박스가 파괴되었음을 설정하고 이를 복제합니다.
 	IsBoxDestroyed = true;
 	OnRep_IsBoxDestroyed();
+	// 보상을 드랍합니다.
 	RewardManager->DropRewardServer(GetActorTransform());	
 }
 
-
+// IsBoxDestroyed 변수가 복제되었을 때 호출됩니다
 void ARewardContainer::OnRep_IsBoxDestroyed()
 {
+	// 이 액터의 수명을 5초로 설정합니다.
 	SetLifeSpan(5.f);
+	// 박스가 파괴된 위치에서 이벤트를 실행합니다.
 	containerDele.ExecuteIfBound(GetActorLocation());
+	// 박스 파괴 사운드를 재생합니다.
 	UGameplayStatics::PlaySoundAtLocation(GetWorld(), ContainerBreakSound, GetActorLocation());
 }
